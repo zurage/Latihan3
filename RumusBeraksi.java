99b92071da7ace3ce758d47c4b2771abc6aa3438
zurage <151634893+zurage@users.noreply.github.com>
2025-06-12 12:30:30 +0800

    Mmebuat class main

---
+++ RumusBeraksi.java
@@ -0,0 +1,18 @@
+
+package latihan3;
+
+
+
+public class RumusBeraksi {
+    public static void main(String[] args) {
+        Rumus orang1 = new Rumus();
+
+        orang1.sisi = 5; 
+        orang1.panjang = 10; 
+        orang1.lebar = 7; 
+
+    
+        orang1.hitungLuasPersegi(); 
+        orang1.hitungLuasPersegiPanjang(); 
+    }
+}
